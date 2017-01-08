16419cd33932356b7624980913701d863cc08d88
Jenny Wong <missjwo@users.noreply.github.com>
2017-01-08 00:20:02 +0000

    made the image naming convention the same

--- index.php
+++ index.php
@@ -4,9 +4,9 @@
 	<meta http-equiv="content-type" content="text/html; charset=UTF-8">
 	<title>Human Made</title>
 
-	<link rel="apple-touch-icon-precomposed" sizes="57x57" href="<?php echo get_stylesheet_directory_uri(); ?>/images/apple-touch-icon-iphone.png" />
-	<link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php echo get_stylesheet_directory_uri(); ?>/images/apple-touch-icon-ipad.png" />
-	<link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php echo get_stylesheet_directory_uri(); ?>/images/apple-touch-icon-iphone-retina-display.png" />
+	<link rel="apple-touch-icon-precomposed" sizes="57x57" href="<?php echo get_stylesheet_directory_uri(); ?>/images/hm-apple-touch-icon-iphone.png" />
+	<link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php echo get_stylesheet_directory_uri(); ?>/images/hm-apple-touch-icon-ipad.png" />
+	<link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php echo get_stylesheet_directory_uri(); ?>/images/hm-apple-touch-icon-iphone-retina-display.png" />
 	<link rel="shortcut icon" type="image/x-icon" href="<?php echo get_stylesheet_directory_uri(); ?>/images/favicon.png" />
 
 	<link type="text/css" rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/style.css">
@@ -16,7 +16,7 @@
 <div class="page-content">
 	<h1>
 		<a href="https://hmn.md" class="company">
-			<img src="<?php echo get_stylesheet_directory_uri(); ?>/images/hm_logo_white.png" alt="Human Made"/>
+			<img src="<?php echo get_stylesheet_directory_uri(); ?>/images/hm-logo-white.png" alt="Human Made"/>
 		</a>
 	</h1>
 </div>
