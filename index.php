ce788cdcf540faf3d12581cf95ebaae701d816a1
Jenny Wong <missjwo@users.noreply.github.com>
2017-01-08 00:02:43 +0000

    adding current files into repo

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html class="bg">
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+	<title>Human Made</title>
+
+	<link rel="apple-touch-icon-precomposed" sizes="57x57" href="https://hmn.md/content/themes/hmnmd/assets/images/apple-touch-icon-iphone.png" />
+	<link rel="apple-touch-icon-precomposed" sizes="72x72" href="https://hmn.md/content/themes/hmnmd/assets/images/apple-touch-icon-ipad.png" />
+	<link rel="apple-touch-icon-precomposed" sizes="114x114" href="https://hmn.md/content/themes/hmnmd/assets/images/apple-touch-icon-iphone-retina-display.png" />
+	<link rel="shortcut icon" type="image/x-icon" href="<?php echo get_stylesheet_directory_uri(); ?>/images/favicon.png" />
+
+	<link type="text/css" rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/style.css">
+	<script src="https://use.typekit.net/yjd8lkq.js"></script>
+	<script>try{Typekit.load({ async: true });}catch(e){}</script>
+</head>
+
+<body>
+<div class="page-content">
+	<h1>
+		<a href="https://hmn.md" class="company">
+			<img src="<?php echo get_stylesheet_directory_uri(); ?>/images/hm_logo_white.png" alt="Human Made"/>
+		</a>
+	</h1>
+</div>
+</body>
+</html>
