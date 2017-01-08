56463905771587929f6d98b7453d414bfa13a61e
Jenny Wong <missjwo@users.noreply.github.com>
2017-01-08 00:11:46 +0000

    Made all images relative

--- index.php
+++ index.php
@@ -4,14 +4,12 @@
 	<meta http-equiv="content-type" content="text/html; charset=UTF-8">
 	<title>Human Made</title>
 
-	<link rel="apple-touch-icon-precomposed" sizes="57x57" href="https://hmn.md/content/themes/hmnmd/assets/images/apple-touch-icon-iphone.png" />
-	<link rel="apple-touch-icon-precomposed" sizes="72x72" href="https://hmn.md/content/themes/hmnmd/assets/images/apple-touch-icon-ipad.png" />
-	<link rel="apple-touch-icon-precomposed" sizes="114x114" href="https://hmn.md/content/themes/hmnmd/assets/images/apple-touch-icon-iphone-retina-display.png" />
+	<link rel="apple-touch-icon-precomposed" sizes="57x57" href="<?php echo get_stylesheet_directory_uri(); ?>/images/apple-touch-icon-iphone.png" />
+	<link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php echo get_stylesheet_directory_uri(); ?>/images/apple-touch-icon-ipad.png" />
+	<link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php echo get_stylesheet_directory_uri(); ?>/images/apple-touch-icon-iphone-retina-display.png" />
 	<link rel="shortcut icon" type="image/x-icon" href="<?php echo get_stylesheet_directory_uri(); ?>/images/favicon.png" />
 
 	<link type="text/css" rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/style.css">
-	<script src="https://use.typekit.net/yjd8lkq.js"></script>
-	<script>try{Typekit.load({ async: true });}catch(e){}</script>
 </head>
 
 <body>
